68e084788c277660f3744b2c7d5cb4c060ab6614
Victor <vx_zz@hotmail.com>
2022-06-16 19:18:55 -0400

    commit tree 61f925dcaeda61c66a24b8990579ad9afc2490d7

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,32 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">
+            Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">
+            Directors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">
+            Actors
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
